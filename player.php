efff096c31fb6eead3052a6fc7c5f4cdca8b6dca
Moon <muziyue@lost9.com>
2014-11-26 18:46:24 +0800

    增加批量获取播放列表歌曲

--- player.php
+++ player.php
@@ -5,8 +5,82 @@
  * Date: 2014/11/25 0025
  * Time: 0:08
  */
-$play_info["cover"] = "cover.jpg";
-$play_info["mp3"] = "mp3.mp3";
-//请clone第一个comit的版本 8f1e33a388168d0b27ced6010aaa56769627fe2a
+include 'list.php';
+
+function curl_get($url)
+{
+    $refer = "http://music.163.com/";
+    $header[] = "Cookie: " . "appver=1.5.0.75771;";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_REFERER, $refer);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+function get_playlist_info($playlist_id)
+{
+    $url = "http://music.163.com/api/playlist/detail?id=" . $playlist_id;
+    return curl_get($url);
+}
+
+function get_music_info($music_id)
+{
+    $url = "http://music.163.com/api/song/detail/?id=" . $music_id . "&ids=%5B" . $music_id . "%5D";
+    return curl_get($url);
+}
+
+function rand_music()
+{
+    global $player_list;
+    $sum = count($player_list);
+    $id = $player_list[rand(0, $sum - 1)];
+    return $id;
+}
+
+function get_music_id()
+{
+    $played = isset($_COOKIE["played"]) ? json_decode($_COOKIE["played"]) : null;
+    $id = rand_music();
+    if ($played != null) {
+        global $player_list;
+        $sum = count($player_list);
+        if ($sum >= 2) {
+            $sum = $sum * 0.5;
+        } else {
+            $sum -= 1;
+        }
+        while (in_array($id, $played)) {
+            $id = rand_music();
+        }
+        if (count($played) >= $sum) {
+            array_splice($played, 0, 1);
+        }
+    }
+    $played[] = $id;
+    setcookie("played", json_encode($played), time() + 3600);
+    return $id;
+}
+
+foreach ($playlist_list as $key) {
+    $json = get_playlist_info($key);
+    $arr = json_decode($json, true);
+    foreach ($arr["result"]["tracks"] as $key2) {
+        $id = $key2["id"];
+        if (!in_array($id, $player_list)) {
+            $player_list[] = $id;
+        }
+    }
+}
+
+$id = get_music_id();
+$music_info = json_decode(get_music_info($id), true);
+$play_info["cover"] = $music_info["songs"][0]["album"]["picUrl"];
+$play_info["mp3"] = $music_info["songs"][0]["mp3Url"];
+$play_info["mp3"] = str_replace("http://m", "http://p", $play_info["mp3"]);
 
 echo json_encode($play_info);
