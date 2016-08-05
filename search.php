f736edb63a920eb1cbc029ad40cfd69da28f1a1e
MinonHeart <git@hub.moe>
2016-08-05 17:53:14 +0800

    Update: remove cookie

--- search.php
+++ search.php
@@ -1,12 +1,12 @@
 <?php
 
-# header("Access-Control-Allow-Headers: Cache-Control, Pragma, Expires");
-# header("Access-Control-Allow-Origin: *");
-# header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Cache-Control, Pragma, Expires");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
-include "config.php";
-include "functions.php";
+include_once "config.php";
+include_once "functions.php";
 
 global $song;
 global $playlist;
@@ -22,8 +22,21 @@ foreach ($playlist as $key) {
     }
 }
 
+function get_music_id()
+{
+    global $song;
+    $sum = count($song);
+    $id = $song[rand(0, $sum - 1)];
+    return $id;
+}
+
 #获取数据
-$id = get_music_id();
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+} else {
+    $id = get_music_id();
+    $play_info["ids"] = $song;
+}
 $music_info = json_decode(get_music_info($id), true);
 $lrc_info = json_decode(get_music_lyric($id), true);
 
@@ -64,4 +77,8 @@ if (isset($lrc_info["lrc"]["lyric"])) {
     $play_info["lrc"] = null;
 }
 
-echo json_encode($play_info);
+if (isset($_GET["callback"])) {
+    echo $_GET["callback"] . "(" . json_encode($play_info) . ")";
+} else {
+    echo json_encode($play_info);
+}
